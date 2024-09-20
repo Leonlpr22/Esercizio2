a789deb12a8b40a6a489db7c265a6bc5c2467605
Benncio09 <lorebencini13@outlook.com>
2024-09-20 09:54:58 +0200

    esercizio-1

---
+++ esercizio1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        
+        echo "<h1>Leon Lo Preiato</h1>";
+
+        $peso = 70;
+        $tassoidratazione =  $peso * 30;
+
+        echo " <p>Il tasso di idratazione di una persona che pesa " . "$peso" . " kg è di " . "$tassoidratazione";
+
+
+
+    ?>
+    
+</body>
+</html>
